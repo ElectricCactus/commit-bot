f02132acf0c33b90e1b8f7000875253e663240aa
Mike Littman <contact@mikelittman.me>
2024-03-23 23:35:35 -0500

    chore(git): add support for base diff in getDiff()
    - Added `base_diff` to the result of `getDiff()`
    - Check for the existence of `GITHUB_BASE_REF` environment variable to determine if a base diff is available
    - Prefer `base_diff` over `cached_diff` and `working_diff` when determining the overall diff
    
    `git git diff github ci`

--- src/git.ts
+++ src/git.ts
@@ -21,11 +21,22 @@ export async function getRepoName() {
 export async function getDiff() {
   const working_diff = await shellText`git diff`
   const cached_diff = await shellText`git diff --cached`
+  let base_diff = ""
+  if (process.env.GITHUB_BASE_REF)
+    base_diff = await shellText`git diff ${process.env.GITHUB_BASE_REF}`
+
   return {
     working_diff,
     cached_diff,
+    base_diff,
+    is_base: base_diff !== "",
     is_cached: cached_diff !== "",
-    diff: cached_diff !== "" ? cached_diff : working_diff,
+    diff:
+      base_diff !== ""
+        ? base_diff
+        : cached_diff !== ""
+          ? cached_diff
+          : working_diff,
   }
 }
 
